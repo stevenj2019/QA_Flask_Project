0339ffaa3c5a3f3363b3af8f41ce32cf1783c701
Steven Johnson <steven.j2019@pm.me>
2020-06-15 22:26:42 +0100

    added test object, script to setup server and pip dependencies

--- Testing/Test-Back-End.py
+++ Testing/Test-Back-End.py
@@ -7,6 +7,7 @@ from flask_testing import TestCase
 from application import app, db, bcrypt
 from application.models import Users, Office_Locations
 
+
 class TestBase(TestCase):
 
     def create_app(self):
@@ -18,6 +19,14 @@ class TestBase(TestCase):
             WTF_CSRF_ENABLES=False,
             DEBUG=True
         )
+        self.user = Users(
+            first_name='john',
+            last_name='doe',
+            phone_number='07627365426'
+            email='john@doe.com', 
+            password=bcrypt.generate_password_hash('ThisPasswordSucks'),
+            office_address='London')
+        )
         return app
 
     def setUp(self):
@@ -26,29 +35,27 @@ class TestBase(TestCase):
         db.drop_all()
         db.create_all()
 
-        admin = Users(
-            first_name='admin',
-            last_name='admin',
-            phone_number='07627892126'
-            email='admin@admin.com', 
-            password=bcrypt.generate_password_hash('admin2016'),
-            office_address='Manchester')
+        print("--------------------------NEXT-TEST----------------------------------------------")
+        chrome_options = Options()
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(executable_path="<PATH TO chromedriver executable>", chrome_options=chrome_options)
+        self.driver.get("http://localhost:5000")
 
-        user = Users(
-            first_name='john',
-            last_name='doe',
-            phone_number='07627365426'
-            email='john@doe.com', 
-            password=bcrypt.generate_password_hash('ThisPasswordSucks'),
-            office_address='London')
-        )
         db.session.add(admin)
         db.session.add(user)
         db.session.commit()
 
         def tearDown(self):
+            self.driver.quit()
+            print("--------------------------END-OF-TEST----------------------------------------------\n\n\n-------------------------UNIT-AND-SELENIUM-TESTS----------------------------------------------")
             db.session.remove()
             db.drop_all()
+
+        def test_server_is_alive(self):
+            response = urlopen("http://localhost:5000")
+            self.assertEqual(response.code, 200)
+   
 
 class TestViews(TestBase):
 
@@ -56,34 +63,20 @@ class TestViews(TestBase):
     def test_auth_view(self):
         self.assertEqual(self.client.get(url_for(auth)), 200)
 
-    def test_home_view_noauth(self):
-        self.assertEqual(self.client.get(url_for(home)), some_int)#find out error status for unauthenticated users
-
-    #authenticated pages
-    def test_home_view_auth(self):
-        self.assertEqual(self.client.get(url_for(home)), 200)
-
 class TestUsers(TestBase):
 
     def test_register(self):
-        return False
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').click()
+        assert bool(Users.query.filter_by(email=self.user.email.first())) == True
     
     def test_login(self):
-        return False
-
-    def test_update(self):
-        return False
-
-    def test_delete(self):
-        return False
-
-class TestOtherUsers(TestBase):
-
-    def test_searchByLocation(self):
-        return False
-
-    def test_searchByName(self):
-        return False
-
-    def test_userProfile(self):
-        return False
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').send_keys()
+        self.driver.find_element_by_xpath('<xpath>').click()
+        assert url_for('home') in self.driver.current_url
---
+++ Tooling/setup.sh
@@ -0,0 +1,18 @@
+if [ "$(whoami)" == "root" ] ; then
+    echo "Setting up your server"
+    sudo DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get upgrade -qq < /dev/null > /dev/null
+    echo "updated server apt cache"
+
+
+    sudo DEBIAN_FRONTEND=noninteractive pip3 install -r requirements.txt < /dev/null > /dev/null
+    echo "Installed pip dependencies"
+    sudo DEBIAN_FRONTEND=noninteractive sudo apt-get install unzip -qq < /dev/null > /dev/null
+    echo "Installed unzip"
+    sudo DEBIAN_FRONTEND=noninteractive sudo apt-get install chromium-browser -qq < /dev/null > /dev/null
+    echo "Installed chromium browser"
+    sudo DEBIAN_FRONTEND=noninteractive wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip < /dev/null > /dev/null
+    unzip chromedriver_linux64.zip 
+    echo "Downloaded and unzipped ChromeDriver for Selenium"
+else
+    echo "Please run as root"
+fi
