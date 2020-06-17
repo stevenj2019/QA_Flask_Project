b7988451af5f43f945488852b7bc1fae4482c5fa
Steven Johnson <steven.j2019@pm.me>
2020-06-17 22:56:53 +0100

    syntax error fix

--- application/forms.py
+++ application/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
 
-class LoginForm(FlaskForm):§
+class LoginForm(FlaskForm):
     email = StringField('Email',
         validators=[
             DataRequired(),
