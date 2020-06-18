5f773eb7722bf09707addd9b5dd2da4d25e0e936
Steven Johnson <steven.j2019@pm.me>
2020-06-18 16:08:15 +0100

    finished home view

--- application/models.py
+++ application/models.py
@@ -12,25 +12,21 @@ class Admin(db.Model, UserMixin):
             'Password Hash', str(self.password), '\n',
         ])
 
-#class Connect(db.Model):
-#    contact_id = db.Column('contact_id', db.Integer, db.ForeignKey('contact.contact_id')),
-#    location_id = db.Column('location_id', db.Integer, db.ForeignKey('locations.location_id'))
+class Contact(db.Model):
+    contact_id = db.Column(db.Integer, primary_key = True)    
+    first_name = db.Column(db.String(10), nullable = False)
+    last_name = db.Column(db.String(20), nullable = False)
+    email_address = db.Column(db.String(30), nullable = False)
+    phone_number = db.Column(db.String(15), nullable = False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'), nullable=False)
 
-#class Contact(db.Model):
-#    contact_id = db.Column(db.Integer, primary_key = True)    
-#    first_name = db.Column(db.String(10), nullable = False)
-#    last_name = db.Column(db.String(20), nullable = False)
-#    email_address = db.Column(db.String(30), nullable = False)
-#    phone_number = db.Column(db.String(15), nullable = False)
-#    office_locations = db.relationship('Locations', secondary='Connect', backref=db.backref('occupants', lazy='dynamic'))
-
-#class Locations(db.Model):
-#    location_id = db.Column(db.Integer, primary_key = True)
-#    first_line = db.Column(db.String(15), nullable = False)
-#    second_line = db.Column(db.String(15), nullable = True)
-#    city = db.Column(db.String(15), nullable = False)
-#    post_code = db.Column(db.String(10), nullable = False)
-#    occupants = db.relationship('Contact', secondary='Connect', backref=db.backref('Address', lazy='dynamic'))
+class Locations(db.Model):
+    location_id = db.Column(db.Integer, primary_key = True)
+    first_line = db.Column(db.String(15), nullable = False)
+    second_line = db.Column(db.String(15), nullable = True)
+    city = db.Column(db.String(15), nullable = False)
+    post_code = db.Column(db.String(10), nullable = False)
+    contact = db.relationship('Contact', backref='location', lazy=True)
 
 @login_manager.user_loader
 def load_user(id):
