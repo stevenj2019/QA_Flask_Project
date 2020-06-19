4bbf218ef816167f38dbaf41bc1d4a4b41633ae7
Steven Johnson <steven.j2019@pm.me>
2020-06-19 18:26:22 +0100

    test updates

--- Testing/test_integration.py
+++ Testing/test_integration.py
@@ -76,4 +76,4 @@ class TestNewContact(TestBase):
         self.driver.find_element_by_xpath('//*[@id="submit"]').click()
         assert url_for('home') in self.driver.current_url
         #tests if appears on the home page
-        assert self.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[1]') == 'John'
+        assert self.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[1]').lower() == 'john' 
