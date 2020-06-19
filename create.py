33cbb5cc69232af25f9623d3b0b35d5859177b51
Steven Johnson <steven.j2019@pm.me>
2020-06-19 12:29:49 +0100

    acting on more errors discovered durting testing

--- create.py
+++ create.py
@@ -1,5 +1,5 @@
 from application import db
-from application.models import Office_Locations, Admin, Contact
+from application.models import Locations, Admin, Contact
 
 db.drop_all()
 db.create_all()
@@ -7,10 +7,11 @@ db.create_all()
 with open("Tooling/office_location_data.csv", "r") as Data:
     for line in Data:
         DATASTREAM = line.split(',')
-        Location_Data = Office_Locations(
-            location = DATASTREAM[0],
-            first_line = DATASTREAM[1], 
-            second_line = DATASTREAM[2], 
+        print(DATASTREAM)
+        Location_Data = Locations(
+            first_line = DATASTREAM[0], 
+            second_line = DATASTREAM[1],
+            city = DATASTREAM[2],
             post_code = DATASTREAM[3],  
         )
         db.session.add(Location_Data)
