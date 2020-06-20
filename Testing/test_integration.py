a61e701977281168cfda9a1b465fc34b1bba1fdf
Steven Johnson <steven.j2019@pm.me>
2020-06-20 13:24:23 +0100

    fixed tests file

--- Testing/test_integration.py
+++ Testing/test_integration.py
@@ -108,5 +108,5 @@ class TestDeleteContact(TestBase):
     def test_deletion(self):
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[7]/a').click()
         time.sleep(5)
-        assert Contact.query.filter_by(contact_id=2).first() == None
+        assert Contact.query.filter_by(contact_id=1).first() == None
     
