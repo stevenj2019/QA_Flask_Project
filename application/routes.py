ae203bbc2d689139e06c044be2779e2e12758eb9
Steven Johnson <steven.j2019@pm.me>
2020-06-20 17:48:00 +0100

    update functionality fix #2

--- application/routes.py
+++ application/routes.py
@@ -62,10 +62,12 @@ def edit(user_id):
         current_data.phone_number = form.phone_number.data
         current_data.location_id = form.city.data
         db.session.query().filter_by(contact_id=user_id).update(
+            dict(
             {Contact.email_address: form.email_address.data},
             {Contact.phone_number: form.phone_number.data},
             {Contact.location_id: form.city.data}
-            )
+            ))
+        db.session.commit()
         return redirect(url_for('home'))
     elif request.method == 'GET':
         form.email_address.data = current_data.email_address
