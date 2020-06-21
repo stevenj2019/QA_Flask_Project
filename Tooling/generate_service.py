a28c9f0a05eebbe0c416ce52017976cd18c36052
Steven Johnson <steven.j2019@pm.me>
2020-06-21 20:51:13 +0100

    updates

--- Tooling/generate_service.py
+++ Tooling/generate_service.py
@@ -1,12 +1,6 @@
 import os 
 import sys 
 
-def getvenvloc(serverFile):
-    List = serverFile.split('/')
-    del List[len(list)-1]
-    List.append('venv/')
-    return '/'.join(List)
-
 def getlogloc(serverFile):
     List = serverFile.split('/')
     del List[len(List)-1]
@@ -24,15 +18,10 @@ def servgen(Description):
             'Description=', Description , '\n',
             '\n',
             '[Service]', '\n',
-            'ExecStart=/bin/sh -c python3 ', getserverloc() , '>', getlogloc(getserverloc()) ,'\n',
+            'EXECStart=/bin/sh -c gunicorn --workers 4 ', getserverloc(), ' > ', getlogloc(getserverloc()),'\n'
             '\n',
             '[Install]', '\n',
             'WantedBy=multi-user.target'
-        ])
-    with open("Flask_Service_Script", "w+") as script:
-        script.writelines([
-            '. ' + getvenvloc + 'bin/activate', '\\n',
-            'python3 ' + getserverloc()
         ])
 
 servgen(sys.argv[1])
--- Tooling/setup.sh
+++ Tooling/setup.sh
@@ -1,25 +1,26 @@
-echo "Preparing Server"
 sudo apt-get update -y 
 sudo apt-get upgrade -y
 sudo apt-get install python3-pip unzip chromium-browser python3-venv -y
 python3 -m venv venv
+
 . venv/bin/activate
 pip3 install -r Tooling/requirements.txt
 
-echo "Testing Software"
-export TEST_DB_URI=''
-export TEST_SECRET_KEY=''
+export TEST_DB_URI=
+export TEST_SECRET_KEY=
+
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-pytest -> This Fails right now 
+pytest --cov-report term-missing --cov=./application
 
-
-echo "Deploying"
-export PROD_DB_URI=''
-export PROD_SECRET_KEY=''
 python3 Tooling/generate_service.py
-cd Tooling
-mv Flask.Service /etc/systemd/system/
-cd ../
+mv Tooling/Flask.service /etc/systemd/system
+
 sudo systemctl deamon-reload
-sudo systemctl start Flask
+sudo sysemctl start Flask
+
+rm -rf Tooling
+rm -rf Documentation_rss
+rm -rf Testing
+rm create.py
+rm README.md
